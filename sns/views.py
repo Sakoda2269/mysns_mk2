f053312606710c4080ce3306c756ca16d864b349
Sakoda2269 <ksakoda.2269@gmail.com>
2023-08-16 16:36:14 +0900

    fix good system

--- sns/views.py
+++ sns/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 
 class IndexView(generic.ListView):
@@ -14,6 +15,10 @@ class IndexView(generic.ListView):
 
     def get_context_data(self, *args, **kwargs: Any):
         context = super().get_context_data(*args, **kwargs)
+        if self.request.user.is_anonymous:
+            context["logined"] = False
+            return context
+        context["logined"] = True
         good = Good.objects.filter(gooder=self.request.user)
         tmp = set()
         for g in good:
@@ -52,6 +57,7 @@ class DeleteView(generic.edit.DeleteView):
     success_url = reverse_lazy("sns:index")
 
 
+@login_required
 def good(request, post_id, isList):
     post = get_object_or_404(Post, id=post_id)
     is_good = Good.objects.filter(gooder=request.user, post=post)
@@ -74,7 +80,6 @@ def good(request, post_id, isList):
 
 def ajax_good(request):
     post_id = request.POST['post_id']
-    print(post_id)
     post = get_object_or_404(Post, id=post_id)
     good = Good.objects.filter(post=post, gooder=request.user)
     context={}
